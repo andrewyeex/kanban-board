4e10e7566b0df9f7ea76d9c7f59cd452015c1d04
Andrew Yee <andrew.yee@mail.com>
2019-02-13 09:14:34 -0800

    add a column fixture

---
+++ src/fixture/columns.js
@@ -0,0 +1,60 @@
+export const columnData = [
+  {
+    id: 1,
+    title: 'dummy',
+    data: [
+      {
+        id: 1,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+      {
+        id: 2,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+      {
+        id: 3,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+      {
+        id: 4,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: 'dummy 2',
+    data: [
+      {
+        id: 5,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+      {
+        id: 6,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: 'dummy 3',
+    data: [
+      {
+        id: 7,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+      {
+        id: 8,
+        dueDate : 1549993211060,
+        description : 'Description'
+      },
+    ]
+  }
+]
